57ec70ee9d6f5b1d737ee9605ae90ab695df267c
jordan <jordan@wncoutdoors.info>
2013-05-02 21:15:22 -0400

    Adds actionbarsherlock lib and some simple tabs on search activity.

--- src/info/wncoutdoors/northcarolinawaterfalls/SearchActivity.java
+++ src/info/wncoutdoors/northcarolinawaterfalls/SearchActivity.java
@@ -2,22 +2,61 @@
 package info.wncoutdoors.northcarolinawaterfalls;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.support.v4.app.FragmentTransaction;
+import android.widget.TextView;
 
-public class SearchActivity extends Activity {
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.app.ActionBar.Tab;
+
+public class SearchActivity extends SherlockActivity implements ActionBar.TabListener {
+    private TextView mSelected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.Theme_Sherlock);
         super.onCreate(savedInstanceState);
+        
         setContentView(R.layout.activity_search);
+        mSelected = (TextView)findViewById(R.id.text);
+        getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        
+        ActionBar.Tab tab1 = getSupportActionBar().newTab();
+        tab1.setText("WATERFALL");
+        tab1.setTabListener(this);
+        getSupportActionBar().addTab(tab1);
+
+        ActionBar.Tab tab2 = getSupportActionBar().newTab();
+        tab2.setText("HIKE");
+        tab2.setTabListener(this);
+        getSupportActionBar().addTab(tab2);
+        
+        ActionBar.Tab tab3 = getSupportActionBar().newTab();
+        tab3.setText("LOCATION");
+        tab3.setTabListener(this);
+        getSupportActionBar().addTab(tab3);
     }
 
+    /*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.search, menu);
         return true;
+    }
+    */
+
+    @Override
+    public void onTabReselected(Tab tab, FragmentTransaction transaction) {
+    }
+
+    @Override
+    public void onTabSelected(Tab tab, FragmentTransaction transaction) {
+        mSelected.setText("Selected: " + tab.getText());
+    }
+
+    @Override
+    public void onTabUnselected(Tab tab, FragmentTransaction transaction) {
     }
 
 }
