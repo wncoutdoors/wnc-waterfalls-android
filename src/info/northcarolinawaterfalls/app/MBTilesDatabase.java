e7432d1001c819180fb97f4d40bbe1d978d034d9
Jordan Mitchell <jordan@wncoutdoors.info>
2014-09-01 10:12:49 -0400

    Reformatting
    Move package declaration outside license boilerplate.

--- src/info/northcarolinawaterfalls/app/MBTilesDatabase.java
+++ src/info/northcarolinawaterfalls/app/MBTilesDatabase.java
@@ -1,5 +1,6 @@
 /*
  * Copyright 2014 WNCOutdoors.info
+ * portions Copyright (C) 2011 readyState Software Ltd, 2007 The Android Open Source Project
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,22 +18,9 @@
  * Class which controls extraction of the MBTiles databases for offline maps
  * and provides access to the resulting file.
  */
-package info.northcarolinawaterfalls.app;/*
 
- * Copyright (C) 2011 readyState Software Ltd, 2007 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
+package info.northcarolinawaterfalls.app;
 
 import android.content.Context;
 import android.os.Environment;
